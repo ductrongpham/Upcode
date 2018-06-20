987ac825504816fcd4b7c1ad9e9a6df3ab43d8ea
ductrongpham <37370091+ductrongpham@users.noreply.github.com>
2018-06-20 07:49:53 +0700

    duc trong

---
+++ myImage.hpp
@@ -0,0 +1,25 @@
+﻿#ifndef _MYIMAGE_
+#define _MYIMAGE_ 
+
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+#include <vector>
+
+using namespace cv;
+using namespace std;
+
+class MyImage{
+
+public:
+	MyImage();
+	MyImage(int webCamera);
+	Mat src; // Load the image
+	Mat srcLR; //ảnh này có kích thước =1/2 ảnh nguồn
+	Mat bw; // Create binary image from source image
+	vector<Mat> bwList; 
+	VideoCapture cap;
+	int cameraSrc;
+	void initWebCamera(int i);
+};
+
+#endif
