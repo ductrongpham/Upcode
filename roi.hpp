6cb68372521a3f211dd2b8a1727d65eea525f5d9
ductrongpham <37370091+ductrongpham@users.noreply.github.com>
2018-06-30 14:39:04 +0700

    a

---
+++ roi.hpp
@@ -0,0 +1,23 @@
+﻿#ifndef _ROI_ 
+#define _ROI_
+
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+class MyRoi{
+public:
+	MyRoi();
+	MyRoi(	Point upperCorner,
+			Point lowerCorner,
+			Mat src);
+	Point upperCorner; //Tọa độ góc cao hơn 
+	Point lowerCorner; //Tọa độ góc thấp hơn
+	Mat roiPtr;
+	Scalar color; //mau sac cua ROI
+	int borderThickness; //kich thuoc duong ve
+	void drawRect(Mat src); //ve ROI tren anh nguon
+};
+
+#endif
